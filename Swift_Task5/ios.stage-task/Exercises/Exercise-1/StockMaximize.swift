823a7ddcae8d523a3f6db0800035636afc55d880
Smi1ezz <ilya.chainikov@mail.ru>
2021-06-13 23:31:50 +0400

    ex1 ex2 done

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,21 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var mySales = [Int]()
+        var lastDayPrice = 0
+        
+        for i in prices {
+            if i != lastDayPrice {
+                while i > lastDayPrice {
+                    lastDayPrice = i
+                    mySales.append(i)
+                }
+            } else {
+                lastDayPrice = i
+            }
+        }
+        var result = 0
+        mySales.forEach { result += lastDayPrice - $0 }
+        return result
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,53 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        
+        var codedMassage = message
+        var decodedMassage: String = ""
+        
+        while codedMassage.count > 0 {
+            var digit = String(codedMassage.prefix(while: {$0 != "["}))
+            if let multiplier = Int(digit) {
+                let firstRange = codedMassage.range(of: digit)
+                codedMassage = codedMassage.replacingCharacters(in: firstRange!, with: "")
+                
+                let subMessage = getText(of: codedMassage)
+                let decryprted = decryptMessage(subMessage)
+                
+                decodedMassage.append(String(repeating: decryprted, count: multiplier))
+                
+                let secondRange = codedMassage.range(of: subMessage)
+                codedMassage = codedMassage.replacingCharacters(in: secondRange!, with: "")
+            } else {
+                digit = String(codedMassage.first!)
+                if digit != "]" && digit != "[" {
+                    decodedMassage.append(digit)
+                }
+                codedMassage = String(codedMassage.dropFirst())
+            }
+        }
+        
+        return decodedMassage
+    }
+    
+    private func getText(of message: String) -> String {
+        
+        var text = ""
+        var counter = 0
+        
+        for i in message {
+            text.append(i)
+            if i != "]" {
+                if i == "[" {
+                    counter += 1
+                }
+            } else {
+                counter -= 1
+                if counter == 0 {
+                    break
+                }
+            }
+        }
+        return text
     }
 }
